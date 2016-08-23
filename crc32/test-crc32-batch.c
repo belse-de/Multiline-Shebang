d2bd5b060108a072afff432be05b6ac53bf4c930
belse-de <belse.de@gmail.com>
2016-08-23 14:40:23 +0200

    fixed asserts; made some symbole name more speaking

--- crc32/test-crc32-batch.c
+++ crc32/test-crc32-batch.c
@@ -22,7 +22,7 @@ typedef char* __isoc_va_list;
 #define log_warn(M, ...)  fprintf(stderr,"[" TERM_YELLOW "WARN " TERM_NONE "] (%s:%d:%s: errno: %s) " M "\n",__FILE__,__LINE__,__FUNC__,clean_errno() , ##__VA_ARGS__)
 #define log_info(M, ...)  fprintf(stderr,"[" TERM_GREEN  "INFO " TERM_NONE "] (%s:%d:%s) " M "\n",__FILE__,__LINE__,__FUNC__ , ##__VA_ARGS__)
 #define log_debug(M, ...) fprintf(stderr,"[" TERM_BLUE   "DEBUG" TERM_NONE "] (%s:%d:%s) " M "\n",__FILE__,__LINE__,__FUNC__ , ##__VA_ARGS__)
-#define check(A, M, ...)  do{if(!(A)) { log_err(M , ##__VA_ARGS__); errno=0; goto error; }}while(0)
+#define check(A, M, ...)  do{if(!(A)) { log_err("" M , ##__VA_ARGS__); errno=0; goto error; }}while(0)
 #define sentinel(M, ...)  do{ log_err(M , ##__VA_ARGS__); errno=0; goto error;}while(0)
 #define check_mem(A)      check((A), "Out of memory.")
 
@@ -31,6 +31,7 @@ typedef char* __isoc_va_list;
 #define CRC32_POLYNOME_REVERSED_RECIPROCAL  0x82608EDB
 #define CRC32_POLYNOME                      CRC32_POLYNOME_NORMAL
 #define BIT(val,pos)                        (((val)&(1<<(pos)))>>(pos))
+#define LENGTH(array)                       sizeof((array))/sizeof((array)[0])
 
 #include <stdlib.h> // exit EXIT_*
 #include <stdio.h>  // *printf
@@ -54,14 +55,14 @@ static inline uint32_t reverse(uint32_t x) {
 /* This is the basic CRC algorithm with no optimizations. It follows the
 logic circuit as closely as possible. */
 uint32_t crc32_verbose(uint8_t *buffer, size_t length) {
-  assert(buffer==NULL || length==0);
+  assert(buffer!=NULL && length!=0);
   uint32_t byte, crc;
 
   crc = 0xFFFFFFFF;
   for (size_t i=0; i<length; i++) {
     byte = buffer[i];            // Get next byte.
     byte = reverse(byte);         // 32-bit reversal.
-    for (uint8_t j = 0; j <bitcount; j++) {    // Do eight times.
+    for (uint8_t j = 0; j<8; j++) {    // Do eight times.
        if ((int)(crc ^ byte) < 0)
             crc = (crc << 1) ^ 0x04C11DB7;
        else crc = crc << 1;
@@ -71,14 +72,14 @@ uint32_t crc32_verbose(uint8_t *buffer, size_t length) {
   return reverse(~crc);
 }
 // same as above, but with given start crc
-uint32_t crc32_verbose_continue(uint32_t start_crc, uint8_t *buf, size_t length) {
-  assert(buffer==NULL || length==0);
+uint32_t crc32_verbose_continue(uint32_t start_crc, uint8_t *buffer, size_t length) {
+  assert(buffer!=NULL && length!=0);
   uint32_t crc,byte;
   crc = reverse(~start_crc);
   for (size_t i=0; i<length; i++) {
-    byte = buf[i];            // Get next byte.
+    byte = buffer[i];            // Get next byte.
     byte = reverse(byte);         // 32-bit reversal.
-    for (uint8_t j = 0; j <bitcount; j++) {    // Do eight times.
+    for (uint8_t j = 0; j <8; j++) {    // Do eight times.
        if ((int)(crc ^ byte) < 0)
             crc = (crc << 1) ^ 0x04C11DB7;
        else crc = crc << 1;
@@ -98,14 +99,14 @@ should be doable in 4 + 61n instructions.
    If the inner loop is strung out (approx. 5*8 = 40 instructions),
 it would take about 6 + 46n instructions. */
 uint32_t crc32_optimized(uint8_t *buffer, size_t length) {
-assert(buffer==NULL || length==0);
+assert(buffer!=NULL && length!=0);
    uint32_t byte, crc, mask;
 
    crc = 0xFFFFFFFF;
    for (size_t i=0; i<length; i++) {
       byte = buffer[i];            // Get next byte.
       crc = crc ^ byte;
-      for (uint8_t j = 0; j <bitcount; j++) {    // Do eight times.
+      for (uint8_t j = 0; j <8; j++) {    // Do eight times.
          mask = -(crc & 1);
          crc = (crc >> 1) ^ (0xEDB88320 & mask);
       }
@@ -113,14 +114,14 @@ assert(buffer==NULL || length==0);
    return ~crc;
 }
 // same as above, but with given start crc
-uint32_t crc32_optimized_continue(uint32_t start_crc, uint8_t *buf, size_t length) {
-  assert(buffer==NULL || length==0);
+uint32_t crc32_optimized_continue(uint32_t start_crc, uint8_t *buffer, size_t length) {
+  assert(buffer!=NULL && length!=0);
   uint32_t crc,byte,mask;
   crc = start_crc ^ ~0;
   for (size_t i=0; i<length; i++) {
-    byte = buf[i];            // Get next byte.
+    byte = buffer[i];            // Get next byte.
     crc = crc ^ byte;
-    for (uint8_t j = 0; j <bitcount; j++) {    // Do eight times.
+    for (uint8_t j = 0; j <8; j++) {    // Do eight times.
        mask = -(crc & 1);
        crc = (crc >> 1) ^ (0xEDB88320 & mask);
     }
@@ -138,7 +139,7 @@ of the 13 or 9 instrucions are load byte.
    This is Figure 14-7 in the text. */
 
 uint32_t crc32_lookup(uint8_t *buffer, size_t length) {
-  assert(buffer==NULL || length==0);
+  assert(buffer!=NULL && length!=0);
   uint32_t byte, crc, mask;
   static uint32_t table[256];
 
@@ -147,7 +148,7 @@ uint32_t crc32_lookup(uint8_t *buffer, size_t length) {
   if (table[1] == 0) {
     for (byte = 0; byte <= 255; byte++) {
        crc = byte;
-       for (uint8_t j = 0; j <bitcount; j++) {    // Do eight times.
+       for (uint8_t j = 0; j <8; j++) {    // Do eight times.
           mask = -(crc & 1);
           crc = (crc >> 1) ^ (0xEDB88320 & mask);
        }
@@ -165,15 +166,15 @@ uint32_t crc32_lookup(uint8_t *buffer, size_t length) {
   return ~crc;
 }
 // same as above, but with given start crc
-uint32_t crc32_lookup_continue(uint32_t start_crc, uint8_t *buf, size_t length) {
-  assert(buffer==NULL || length==0);
+uint32_t crc32_lookup_continue(uint32_t start_crc, uint8_t *buffer, size_t length) {
+  assert(buffer!=NULL && length!=0);
   static uint32_t table[256];
-  uint32_t crc_tmp,mask,crc,byte
+  uint32_t crc_tmp,mask,crc,byte;
   /* Set up the table, if necessary. */
   if (table[1] == 0) {
     for (uint32_t byte = 0; byte <= 255; byte++) {
        crc_tmp = byte;
-       for (uint8_t j = 0; j <bitcount; j++) {    // Do eight times.
+       for (uint8_t j = 0; j <8; j++) {    // Do eight times.
           mask = -(crc_tmp & 1);
           crc_tmp = (crc_tmp >> 1) ^ (0xEDB88320 & mask);
        }
@@ -184,7 +185,7 @@ uint32_t crc32_lookup_continue(uint32_t start_crc, uint8_t *buf, size_t length) 
   /* Through with table setup, now calculate the CRC. */
   crc = ~start_crc;
   for (size_t i=0; i<length; i++) {
-    byte = buf[i];
+    byte = buffer[i];
     crc = (crc >> 8) ^ table[(crc ^ byte) & 0xFF];
   }
   return ~crc;
@@ -197,14 +198,19 @@ uint32_t crc32_lookup_continue(uint32_t start_crc, uint8_t *buf, size_t length) 
 
 /* ---- main ---- */
 int main(void) {
-  size_t runs = 100;
-  size_t buffer_len = 3;
+  size_t runs = 1;
+  size_t buffer_len = 4;
   uint8_t buffer[buffer_len];
 
-
-  for(size_t run=0; run<runs; run++){
-    log_info("Run: %4lu %%%3lu", run, run*100/runs);
+    #if 0
+          
+        if(buffer[cell_pos]%128==0){
+          log_info("rsp: %d %3d %3d", run, buf_step, cell_pos);
+        }
+    #endif
     
+  for(size_t run=0; run<runs; run++){
+    log_info("Run: %4lu %%%3lu", run, run*100/runs); 
     for( size_t buf_step=0; buf_step<buffer_len; buf_step++){
       for(size_t cell_pos=0; cell_pos<=buf_step;){
         
@@ -215,49 +221,56 @@ int main(void) {
           buffer[cell_pos] += 1;
           cell_pos = 0;
         }
+
+
+        //uint32_t crc_v  = crc32_verbose(buffer, buf_step+1);
+        //uint32_t crc_vc = crc32_verbose_continue(0, buffer, buf_step+1);
+        //uint32_t crc_o  = crc32_optimized(buffer, buf_step+1);
+        //uint32_t crc_oc = crc32_optimized_continue(0, buffer, buf_step+1);
+        uint32_t crc_l  = crc32_lookup           (buffer, buf_step+1);
+        uint32_t crc_lc = crc32_lookup_continue(0,buffer, buf_step+1);
         
-        bool error_found = false;
-        for(int i=0; func_array[i]; i++)
-        {
-          func32_res[i] = func_array[i](0,buffer,buffer_len);
-          if( i>1 && (func32_res[i-1]!=func32_res[i]) ) { error_found = true; }
-        }
+        uint32_t crc = crc_l;
+        //check((crc == crc_v ), "miss match crc_v  exp:%08x got:%08x", crc, crc_v);
+        //check((crc == crc_vc), "miss match crc_vc exp:%08x got:%08x", crc, crc_vc);
+        //check((crc == crc_o ), "miss match crc_o  exp:%08x got:%08x", crc, crc_o);
+        //check((crc == crc_oc), "miss match crc_oc exp:%08x got:%08x", crc, crc_oc);
+        check((crc == crc_l ), "miss match crc_l  exp:%08x got:%08x", crc, crc_l);
+        check((crc == crc_lc), "miss match crc_lc exp:%08x got:%08x", crc, crc_lc);
         
-        if( error_found 
-            || (func32_res[0] == 0xffffffff) 
-            || (func32_res[0] == 0x00000000) )
+        if( (crc == 0xffffffff) || (crc == 0x00000000) )
         {
-          log_err("Calced crc does not match");
+          log_info("nice crc: %08x", crc);
           for( size_t print_pos=0; print_pos<buffer_len; print_pos++)
           {
-             printf("%02x ", buffer[print_pos]);
-          }printf("\n");
-            
-          for(int k=0; func_array[k]; k++){
-            printf("[%d] crc32%c: 0x%08x\n", k, 'a'+k, func32_res[k]);
-          }
+             fprintf(stderr, "%02x ", buffer[print_pos]);
+          }fprintf(stderr,"\n");
+
         }
       }
     }
   }
+  
   const char * str_v0    = "Hallo World!";
   size_t   str_len_v0 = strlen(str_v0);
-  uint32_t crc_v0 = func_array[0](0,     (uint8_t*)str_v0,str_len_v0);
+  uint32_t crc_v0 = crc32_verbose_continue(0,     (uint8_t*)str_v0,str_len_v0);
   
-  uint32_t crc_v1 = func_array[5](0,(uint8_t*)str_v0,str_len_v0-6);
-  uint32_t crc_v2 = func_array[5](crc_v1,(uint8_t*)(&str_v0[6]),str_len_v0-6);
-  log_debug("crc32: 0x%08x   0x%08x", crc_v0, crc_v2);
+  uint32_t crc_v1 = crc32_verbose_continue(0,(uint8_t*)str_v0,str_len_v0-6);
+  uint32_t crc_v2 = crc32_verbose_continue(crc_v1,(uint8_t*)(&str_v0[6]),str_len_v0-6);
   check(crc_v0 == crc_v2,"continuation did not work");
   
-  uint32_t crc_v3 = func_array[4](0,(uint8_t*)str_v0,str_len_v0-6);
-  uint32_t crc_v4 = func_array[4](crc_v3,(uint8_t*)(&str_v0[6]),str_len_v0-6);
-  log_debug("crc32: 0x%08x   0x%08x", crc_v0, crc_v4);
+  uint32_t crc_v3 = crc32_optimized_continue(0,(uint8_t*)str_v0,str_len_v0-6);
+  uint32_t crc_v4 = crc32_optimized_continue(crc_v3,(uint8_t*)(&str_v0[6]),str_len_v0-6);
   check(crc_v0 == crc_v4,"continuation did not work");
   
-  uint32_t crc_v5 = func_array[3](0,(uint8_t*)str_v0,str_len_v0-6);
-  uint32_t crc_v6 = func_array[3](crc_v5,(uint8_t*)(&str_v0[6]),str_len_v0-6);
-  log_debug("crc32: 0x%08x   0x%08x", crc_v0, crc_v6);
+  uint32_t crc_v5 = crc32_lookup_continue(0,(uint8_t*)str_v0,str_len_v0-6);
+  uint32_t crc_v6 = crc32_lookup_continue(crc_v5,(uint8_t*)(&str_v0[6]),str_len_v0-6);
   check(crc_v0 == crc_v6,"continuation did not work");
+  
+  uint8_t nice1[] = {0xff,0xff,0xff,0xff}; 
+  uint8_t nice2[] = {0x9d,0x0a,0xd9,0x6d};
+  
+  uint32_t nice1_crc = crc32_verbose(0,nice1,LENGTH(nice1));
     
   exit(EXIT_SUCCESS);
 error:
